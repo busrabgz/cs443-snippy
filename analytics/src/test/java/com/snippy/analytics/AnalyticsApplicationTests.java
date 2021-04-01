47946c7a82f41942e3f558cfab1f49d93068dbd5
Doğaç Eldenk <dogacel@gmail.com>
2021-04-01 23:35:54 +0300

    Add submodules

---
+++ analytics/src/test/java/com/snippy/analytics/AnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snippy.analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ auth/src/test/java/com/snippy/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snippy.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

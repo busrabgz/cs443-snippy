3eea093a6d7e66931a28b1895c21b4f8425668d1
Doğaç Eldenk <dogacel@gmail.com>
2021-04-20 01:25:37 +0300

    Fix eu.gpr repo names

--- scripts/image_update.sh
+++ scripts/image_update.sh
@@ -5,11 +5,13 @@ export PROJECT_ID=snippy-me-cs443
 gcloud container clusters get-credentials snippy-cluster
 
 pushd ../core
-docker build -t gcr.io/${PROJECT_ID}/cs443-snippy_app:${VERSION} .
+docker build -t eu.gcr.io/${PROJECT_ID}/cs443-snippy_app:${VERSION} -t eu.gcr.io/${PROJECT_ID}/cs443-snippy_app:latest .
 
 pushd ../analytics
-docker build -t gcr.io/${PROJECT_ID}/cs443-snippy_analytics:${VERSION} . 
+docker build -t eu.gcr.io/${PROJECT_ID}/cs443-snippy_analytics:${VERSION} -t eu.gcr.io/${PROJECT_ID}/cs443-snippy_analytics:latest . 
 
 docker push eu.gcr.io/${PROJECT_ID}/cs443-snippy_app:${VERSION}
+docker push eu.gcr.io/${PROJECT_ID}/cs443-snippy_app:latest
 docker push eu.gcr.io/${PROJECT_ID}/cs443-snippy_analytics:${VERSION}
+docker push eu.gcr.io/${PROJECT_ID}/cs443-snippy_analytics:latest
 

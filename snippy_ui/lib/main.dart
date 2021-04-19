82a72089d659410e9c7a13a956300480d39f58ed
Ozan Aydın <ozanaydinn07@gmail.com>
2021-04-19 20:04:07 +0300

    added main_screen

--- snippy_ui/lib/main.dart
+++ snippy_ui/lib/main.dart
@@ -2,22 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:snippy_ui/screens/Welcome/welcome_screen.dart';
 import 'package:snippy_ui/screens/Login/login_screen.dart';
 import 'package:snippy_ui/screens/Register/register_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:snippy_ui/screens/wrapper.dart';
+import 'package:provider/provider.dart';
+import 'package:snippy_ui/services/auth.dart';
 
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Snippy',
-      theme: ThemeData(
-        primaryColor: Colors.red,
-        scaffoldBackgroundColor: Colors.white,
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+      home: Wrapper()
       ),
-      home: WelcomeScreen(),
     );
   }
 }

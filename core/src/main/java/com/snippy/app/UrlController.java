766b7da26bb360d727015636c4ab2cb53022e77e
Doğaç Eldenk <dogacel@gmail.com>
2021-04-22 03:12:23 +0300

    Update URL Controller.

--- core/src/main/java/com/snippy/app/UrlController.java
+++ core/src/main/java/com/snippy/app/UrlController.java
@@ -4,34 +4,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.Parameter;
-
 import org.apache.commons.validator.routines.UrlValidator;
 
 import org.springframework.http.MediaType;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseToken;
-import com.google.firebase.auth.internal.FirebaseTokenFactory;
-import com.google.firebase.auth.internal.FirebaseTokenVerifier;
 import com.snippy.libs.Url;
-
 
 import static com.snippy.libs.Config.getDb;
 import static com.snippy.libs.Config.getJedis;
 
-import java.lang.reflect.Array;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.print.attribute.standard.Media;
 
 @RestController
 public class UrlController {
@@ -49,8 +39,25 @@ public class UrlController {
 		return shortUrl.getUrl();
 	}
 
-	@RequestMapping(value="/urls", method = RequestMethod.POST, consumes = MediaType.TEXT_PLAIN_VALUE)
-	public String create(@RequestBody String url) {
+	@GetMapping("/u/{id}")
+	public ResponseEntity<Void> redirectToURL(@PathVariable String id) throws Exception {
+		String actualUrl = getUrl(id);
+		String redirectUrl = "redirect:" + actualUrl == null ? "https://www.cloudflare.com/404/" : actualUrl;
+		return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(redirectUrl)).build();
+	}
+
+	@GetMapping("/urls/{id}")
+	public ResponseEntity<String> getUrlForId(@PathVariable String id) {
+		try {
+			String shortUrl = getUrl(id);
+			return ResponseEntity.status(HttpStatus.OK).body(shortUrl);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("");
+		}
+	}
+
+	@RequestMapping(value = "/urls", method = RequestMethod.POST, consumes = MediaType.TEXT_PLAIN_VALUE)
+	public ResponseEntity<String> create(@RequestBody String url) {
 		var db = getDb();
 		var jedis = getJedis();
 
@@ -59,69 +66,57 @@ public class UrlController {
 
 		UrlValidator urlValidator = new UrlValidator(new String[] { "http", "https" });
 		if (!urlValidator.isValid(url)) {
-			return "Url is not valid.";
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid URL.");
 		}
 
 		Url shortUrl = Url.create(url);
 
-		System.out.println("GOT:" + shortUrl.getId());
 		jedis.set(shortUrl.getId(), shortUrl.getUrl());
-
 		db.document("urls/" + shortUrl.getId()).create(shortUrl);
-
-		return shortUrl.getId();
+		return ResponseEntity.status(HttpStatus.OK).body(shortUrl.getId());
 	}
 
-	@RequestMapping(value="/namedUrls", method = RequestMethod.POST)
+	@RequestMapping(value = "/namedUrls", method = RequestMethod.POST)
 	public ResponseEntity<String> create(@RequestBody Url url, @RequestHeader("fa-auth") String auth) {
 		var db = getDb();
 		var jedis = getJedis();
 
-
-		System.out.println(url.getId() + " " + url.getUrl());
+		try {
+			FirebaseAuth.getInstance().verifyIdToken(auth);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("");
+		}
 
 		UrlValidator urlValidator = new UrlValidator(new String[] { "http", "https" });
 		if (!urlValidator.isValid(url.getUrl())) {
-			return new ResponseEntity<String>("Url is not valid.", HttpStatus.BAD_REQUEST);
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid URL.");
 		}
 
-		try { 
+		try {
 			getUrl(url.getId());
-	
-			return new ResponseEntity<String>("Url already taken.", HttpStatus.BAD_REQUEST);
+
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("URL name already exists.");
 		} catch (Exception e) {
 			jedis.set(url.getId(), url.getUrl());
 			db.document("urls/" + url.getId()).create(url);
 
-			return new ResponseEntity<String>(url.getId(), HttpStatus.OK);
+			return ResponseEntity.status(HttpStatus.OK).body(url.getId());
 		}
 
 	}
 
-	@GetMapping("/urls")
-	public List<String> getUrlForUser(@RequestHeader("fa-auth") String auth) throws Exception {
-		
-		var retList = new ArrayList<String>(); 
+	@GetMapping("/userUrls")
+	public ResponseEntity<List<String>> getUrlForUser(@RequestHeader("fa-auth") String auth) throws Exception {
 
-		FirebaseToken token = FirebaseAuth.getInstance().verifyIdToken(auth);
+		try {
+			FirebaseAuth.getInstance().verifyIdToken(auth);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+		}
 
-		System.out.println("Auth is: " + auth);
+		var retList = new ArrayList<String>();
 
-
-
-		return retList;
+		return ResponseEntity.status(HttpStatus.OK).body(retList);
 	}
 
-	@GetMapping("/urls/{id}")
-	public String getUrlForId(@PathVariable String id) throws Exception {
-		String shortUrl = getUrl(id);
-		return shortUrl == null ? "No such url exists." : shortUrl;
-	}
-
-	@GetMapping("/u/{id}")
-	public ResponseEntity<Void> redirectToURL(@PathVariable String id) throws Exception {		
-		String actualUrl = getUrl(id);
-		String redirectUrl = "redirect:" + actualUrl == null ? "https://www.cloudflare.com/404/" : actualUrl;
-		return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(redirectUrl)).build();
-	}
 }
